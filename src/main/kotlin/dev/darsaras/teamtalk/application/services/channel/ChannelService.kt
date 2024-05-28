fe9ff6f517e6c56d9810de13ccb2c397567fd66f
DanteDeLordran <lordran@lordran.dev>
2024-05-27 18:01:03 -0600

    Mistyped return value in services

--- src/main/kotlin/dev/darsaras/teamtalk/application/services/channel/ChannelService.kt
+++ src/main/kotlin/dev/darsaras/teamtalk/application/services/channel/ChannelService.kt
@@ -1,12 +1,13 @@
 package dev.darsaras.teamtalk.application.services.channel
 
 import dev.darsaras.teamtalk.domain.models.channel.requests.ChannelRequest
+import dev.darsaras.teamtalk.domain.models.channel.responses.ChannelResponse
 import org.springframework.http.ResponseEntity
 
 interface ChannelService {
     fun createChannel( request : ChannelRequest ) : ResponseEntity<Unit>
-    fun getChannel( id : Long ) : ResponseEntity<Unit>
-    fun getAllChannels( groupId : Long ) : ResponseEntity<Unit>
+    fun getChannel( id : Long ) : ResponseEntity<ChannelResponse>
+    fun getAllChannels( groupId : Long ) : ResponseEntity<List<ChannelResponse>>
     fun deleteChannel( id : Long ) : ResponseEntity<Unit>
     fun changeChannelName( id : Long , name : String ) : ResponseEntity<Unit>
 }
--- src/main/kotlin/dev/darsaras/teamtalk/application/services/user/UserService.kt
+++ src/main/kotlin/dev/darsaras/teamtalk/application/services/user/UserService.kt
@@ -1,11 +1,12 @@
 package dev.darsaras.teamtalk.application.services.user
 
 import dev.darsaras.teamtalk.domain.models.user.requests.UserRequest
+import dev.darsaras.teamtalk.domain.models.user.responses.UserResponse
 import org.springframework.http.ResponseEntity
 
 interface UserService {
     fun createUser(request : UserRequest) : ResponseEntity<Unit>
-    fun getUser(id : Long) : ResponseEntity<Unit>
+    fun getUser(id : Long) : ResponseEntity<UserResponse>
     fun updateUser( request: UserRequest , id : Long ): ResponseEntity<Unit>
     fun deleteUser( id: Long ) : ResponseEntity<Unit>
 }
