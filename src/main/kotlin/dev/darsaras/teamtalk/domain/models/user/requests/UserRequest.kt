e347778e73048244555bfa0358fb3e3c55fcd2c2
DanteDeLordran <lordran@lordran.dev>
2024-05-29 14:47:31 -0600

    Made Kotlin compatible with Jakarta Validations

--- src/main/kotlin/dev/darsaras/teamtalk/domain/models/user/requests/UserRequest.kt
+++ src/main/kotlin/dev/darsaras/teamtalk/domain/models/user/requests/UserRequest.kt
@@ -3,29 +3,30 @@ package dev.darsaras.teamtalk.domain.models.user.requests
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 
 @Schema(
     description = "Request schema for User"
 )
 data class UserRequest(
     @Schema(example = "Dante")
-    @NotEmpty(message = "Name can not be null or empty")
-    val name : String,
+    @field:NotEmpty(message = "Name can not be null or empty")
+    val name : String?,
 
     @Schema(example = "de Lordran")
-    @NotEmpty(message = "Lastname can not be null or empty")
-    val lastname : String,
+    @field:NotEmpty(message = "Lastname can not be null or empty")
+    val lastname : String?,
 
     @Schema(example = "Lordran")
-    @NotEmpty(message = "Username can not be null or empty")
-    val username : String,
+    @field:NotEmpty(message = "Username can not be null or empty")
+    val username : String?,
 
     @Schema(example = "asdasdasdasdsa")
-    @NotEmpty(message = "Password can not be null or empty")
-    val password : String,
+    @field:NotEmpty(message = "Password can not be null or empty")
+    val password : String?,
 
     @Schema(example = "lordran@lordran.dev")
-    @Email(message = "Email address not valid")
-    @NotEmpty(message = "Email can not be null or empty")
-    val email : String
+    @field:Email(message = "Email address not valid")
+    @field:NotEmpty(message = "Email can not be null or empty")
+    val email : String?
 )
