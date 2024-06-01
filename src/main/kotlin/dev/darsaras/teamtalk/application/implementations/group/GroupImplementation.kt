45fa498a39adf3a83913aef6db7775590c236a53
Lordran <lordran@lordran.dev>
2024-06-01 00:15:18 -0700

    Refactored add and remove members from Group methods , also now when a user creates a group , user is automatically added to it

--- src/main/kotlin/dev/darsaras/teamtalk/application/implementations/group/GroupImplementation.kt
+++ src/main/kotlin/dev/darsaras/teamtalk/application/implementations/group/GroupImplementation.kt
@@ -6,7 +6,9 @@ import dev.darsaras.teamtalk.domain.models.group.Group
 import dev.darsaras.teamtalk.domain.models.group.requests.GroupRequest
 import dev.darsaras.teamtalk.domain.models.group.responses.GroupResponse
 import dev.darsaras.teamtalk.domain.models.user.User
+import dev.darsaras.teamtalk.domain.models.user.responses.UserResponse
 import dev.darsaras.teamtalk.domain.repositories.group.GroupRepository
+import dev.darsaras.teamtalk.domain.repositories.user.UserRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
@@ -14,9 +16,11 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 
 @Service
-class GroupImplementation(private val groupRepository: GroupRepository) : GroupService{
+class GroupImplementation(private val groupRepository: GroupRepository, private val userRepository: UserRepository) : GroupService{
 
-    override fun createGroup(request: GroupRequest): ResponseEntity<Unit> {
+    override fun createGroup(id: Long, request: GroupRequest): ResponseEntity<Unit> {
+        val user = userRepository.findById(id)
+        if (user.isEmpty) throw ResourceNotFoundException(resourceName = "User", fieldName = "id", fieldValue = "$id")
         val foundGroup = groupRepository.findByName(request.name)
         if (foundGroup != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
@@ -24,7 +28,7 @@ class GroupImplementation(private val groupRepository: GroupRepository) : GroupS
             val group = Group(
                 name = request.name,
                 createdAt = ZonedDateTime.now(ZoneId.of("America/Mexico_City")).toLocalDateTime(),
-                users = mutableSetOf()
+                users = mutableSetOf(user.get())
             )
             groupRepository.save(group)
             return ResponseEntity.status(HttpStatus.CREATED).build()
@@ -73,19 +77,29 @@ class GroupImplementation(private val groupRepository: GroupRepository) : GroupS
         }else throw ResourceNotFoundException(fieldName = "id", fieldValue = "$id", resourceName = "Group")
     }
 
-    override fun addMembers(id: Long, members: Set<User>): ResponseEntity<Unit> {
+    override fun addMembers(id: Long, members: Set<UserResponse>): ResponseEntity<Unit> {
         val group = groupRepository.findById(id)
+        val users = mutableSetOf<User>()
+        for (response in members){
+            val user = userRepository.findById(response.id)
+            if (user.isPresent) users.add(user.get())
+        }
         if (group.isPresent){
-            group.get().users.addAll(members)
+            group.get().users.addAll(users)
             groupRepository.save(group.get())
             return ResponseEntity.status(HttpStatus.OK).build()
         }else throw ResourceNotFoundException(fieldName = "id", fieldValue = "$id", resourceName = "Group")
     }
 
-    override fun removeMembers(id: Long, members: Set<User>): ResponseEntity<Unit> {
+    override fun removeMembers(id: Long, members: Set<UserResponse>): ResponseEntity<Unit> {
         val group = groupRepository.findById(id)
+        val users = mutableSetOf<User>()
+        for (response in members){
+            val user = userRepository.findById(response.id)
+            if (user.isPresent) users.add(user.get())
+        }
         if (group.isPresent){
-            group.get().users.removeAll(members)
+            group.get().users.removeAll(users)
             groupRepository.save(group.get())
             return ResponseEntity.status(HttpStatus.OK).build()
         }else throw ResourceNotFoundException(fieldName = "id", fieldValue = "$id", resourceName = "Group")
--- src/main/kotlin/dev/darsaras/teamtalk/application/services/group/GroupService.kt
+++ src/main/kotlin/dev/darsaras/teamtalk/application/services/group/GroupService.kt
@@ -3,14 +3,16 @@ package dev.darsaras.teamtalk.application.services.group
 import dev.darsaras.teamtalk.domain.models.group.requests.GroupRequest
 import dev.darsaras.teamtalk.domain.models.group.responses.GroupResponse
 import dev.darsaras.teamtalk.domain.models.user.User
+import dev.darsaras.teamtalk.domain.models.user.requests.UserRequest
+import dev.darsaras.teamtalk.domain.models.user.responses.UserResponse
 import org.springframework.http.ResponseEntity
 
 interface GroupService {
-    fun createGroup( request : GroupRequest ) : ResponseEntity<Unit>
+    fun createGroup( id : Long ,request : GroupRequest ) : ResponseEntity<Unit>
     fun getGroup( id : Long ) : ResponseEntity<GroupResponse>
     fun getAllGroups( userId : Long ) : ResponseEntity<List<GroupResponse>>
     fun deleteGroup( id : Long ) : ResponseEntity<Unit>
     fun changeGroupName( id : Long , name : String ) : ResponseEntity<Unit>
-    fun addMembers( id : Long , members : Set<User> ) : ResponseEntity<Unit>
-    fun removeMembers( id : Long , members : Set<User> ) : ResponseEntity<Unit>
+    fun addMembers( id : Long , members : Set<UserResponse> ) : ResponseEntity<Unit>
+    fun removeMembers( id : Long , members : Set<UserResponse> ) : ResponseEntity<Unit>
 }
