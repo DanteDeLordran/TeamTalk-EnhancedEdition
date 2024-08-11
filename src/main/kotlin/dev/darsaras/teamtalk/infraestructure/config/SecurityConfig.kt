f5638662f63cada21e7384fbe8a1a8fe5067c20f
Lordran <lordran@lordran.dev>
2024-08-11 14:15:29 -0600

    Password encoder

--- src/main/kotlin/dev/darsaras/teamtalk/infraestructure/config/SecurityConfig.kt
+++ src/main/kotlin/dev/darsaras/teamtalk/infraestructure/config/SecurityConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -26,6 +28,11 @@ class SecurityConfig {
             formLogin { disable() }
         }
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder() : PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
     }
 
 }
